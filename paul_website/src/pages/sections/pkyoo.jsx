8dadc9b7cf16b6c0f9f70ae70ecfd696ace77892
PKYOO-116 <pkyoo116@gmail.com>
2025-09-27 00:52:20 -0700

    Pkyoo Update

--- paul_website/src/pages/sections/pkyoo.jsx
+++ paul_website/src/pages/sections/pkyoo.jsx
@@ -48,7 +48,7 @@ export default function Pkyoo() {
                 words={[Name]}
                 typeSpeed={50}
                 deleteSpeed={0}
-                delaySpeed={400}
+                delaySpeed={200}
                 cursor
                 onType={(char) => {
                   setTypedLength((current) => {
@@ -68,7 +68,7 @@ export default function Pkyoo() {
         <div className="grid grid-7"></div>
         <div className="grid grid-8">
           {doneTyping && (
-            <div className="pkyoo__flags">
+            <div className={`pkyoo__flags ${doneTyping ? "visible" : ""}`}>
               <div className="pkyoo__flagBox">
                 <div className="pkyoo__flagLabel">U.S.</div>
                 <img src={flagUS} alt="USflag" className="pkyoo__flag" />
